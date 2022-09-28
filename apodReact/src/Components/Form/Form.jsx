6705c466181073ecf61c2f6707f37939587f1b5f
Willian Carvalho da Silva <wcarvalho91610@gmail.com>
2022-09-28 17:29:57 -0300

    criei o projeto da apod com vite e coloquei os componentes header e input

---
+++ apodReact/src/Components/Form/Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <form action="">
+        
+    </form>
+  )
+}
+
+export default Form
---
+++ apodReact/src/routes.jsx
@@ -0,0 +1,13 @@
+
+import Header from './Components/Header/Header'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  )
+}
+
+export default App
